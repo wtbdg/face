bb87009d6016f92b18b47f8835cad78db1c62859
Rio Purnomo <siriokun@gmail.com>
2022-01-27 20:35:03 +0700

    fullscreen

--- server.js
+++ server.js
@@ -27,25 +27,23 @@ video.addEventListener('play', () => {
             //console.log(detections);
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-        const expressions = null;
+            // faceapi.draw.drawDetections(canvas, resizedDetections)
+            // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
         const happy = resizedDetections[0].expressions.happy;
         const neutral = resizedDetections[0].expressions.neutral;
         const angry = resizedDetections[0].expressions.angry;
         const surprised = resizedDetections[0].expressions.surprised;
         if (happy > angry && happy > neutral && happy > surprised) {
-            document.body.style.backgroundColor = "green";
-            document.getElementById('message').innerHTML = "YOU ARE HAPPY"
+            document.getElementById('message').classList = "state-happy"
+                // document.getElementById('message').innerHTML = "YOU ARE HAPPY"
         } else if (angry > happy && angry > neutral && angry > surprised) {
-            document.body.style.backgroundColor = "red";
-            document.getElementById('message').innerHTML = "YOU ARE ANGRY"
+            document.getElementById('message').classList = "state-angry"
+                // document.getElementById('message').innerHTML = "YOU ARE ANGRY"
         } else if (surprised > happy && surprised > neutral && surprised > angry) {
-            document.body.style.backgroundColor = "yellow";
-            document.getElementById('message').innerHTML = "YOU ARE SURPRISED";
+            document.getElementById('message').classList = "state-surprised"
+                // document.getElementById('message').innerHTML = "YOU ARE SURPRISED"
         } else {
-            document.body.style.backgroundColor = "white";
-            document.getElementById('message').innerHTML = "YOU ARE NEUTRAL";
+            document.getElementById('message').classList = ""
         }
 
     }, 100)
